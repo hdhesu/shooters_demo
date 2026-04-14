f6545dd98dcba71c3765ad18fa230e872431fe68
Henri <henri.sutinen@tuni.fi>
2026-04-14 15:49:48 +0300

    switched keyboardmovement script to match unitys CharacterController documentation

--- Assets/Scripts/KeyboardMovement.cs
+++ Assets/Scripts/KeyboardMovement.cs
@@ -1,17 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.XR;
 
 public class KeyboardMovement : MonoBehaviour
 {
-    public float movementSpeed = 5f;
-    float gravity = -9.81f * 2;
+    private float movementSpeed = 5.0f;
+    private float gravityValue = -9.81f;
+    private float jumpHeight = 1.5f;
 
-    private Vector2 playerMoveInput;
-    private Vector3 velocity;
+    private bool groundedPlayer;
+    private Vector3 playerVelocity;
     private CharacterController characterController;
 
+    [Header("Input Actions")]
+    public InputActionReference moveAction; // expects Vector2
+    public InputActionReference jumpAction; // expects Button
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
         characterController = GetComponent<CharacterController>();
     }
@@ -19,25 +25,35 @@ public class KeyboardMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        groundedPlayer = characterController.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+
+        // Read input
+        Vector2 input = moveAction.action.ReadValue<Vector2>();
+        Vector3 move = new Vector3(input.x, 0, input.y);
+        move = Vector3.ClampMagnitude(move, 1f);
+
+        if (move != Vector3.zero)
+        {
+            transform.forward = move;
+        }
+
+        // Jump
+        if (jumpAction.action.triggered && groundedPlayer)
+        {
+            playerVelocity.y = Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+        }
+
+        // Apply gravity
+        playerVelocity.y += gravityValue * Time.deltaTime;
+
+        // Combine horizontal and vertical movement
+        Vector3 finalMove = (move * movementSpeed) + (playerVelocity.y * Vector3.up);
+        characterController.Move(finalMove * Time.deltaTime);
     }
 
-    void FixedUpdate()
-    {
-        MovePlayer();
-    }
-
-    void OnMove(InputValue value)
-    {
-        playerMoveInput = value.Get<Vector2>();
-    }
-
-    void MovePlayer()
-    {
-        Vector3 direction = transform.right * playerMoveInput.x + transform.forward * playerMoveInput.y;
-        characterController.Move(direction.normalized * movementSpeed * Time.deltaTime);
-
-        velocity.y += gravity * Time.deltaTime;
-        characterController.Move(velocity * Time.deltaTime);
-    }
+    
 }
